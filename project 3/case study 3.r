a17e9fded09b7f5c9c24879911fc178c7c9196c3
unknown <RTL Tracking@Tracking.ROY.LOCAL>
2017-05-15 07:07:34 +0300

    output to pdf

--- project 3/case study 3.r
+++ project 3/case study 3.r
@@ -1,5 +1,6 @@
 #QUESTION 1
-# Consider following values for the purpose of this project
+#SOLUTION
+#values for the purpose of this project
 iprice=10        #the stock price at t
 return = 0.15    #expected annual stock return
 volatility = 0.2 #the annualized volatility of the underlying stock
@@ -8,6 +9,8 @@ steps = 100      #the number of steps involved in calculation
 term = rnorm(1) #distribution term 
 
 
+
+#QUESTION 2
 #SOLUTION
 v_steps = c() #a vector to store each successive step
 count = 0 #keeps track of number of steps
@@ -16,7 +19,8 @@ fprice = 0 #stock price at each time intervals
 #A function to calculate the stock price at st + n
 stock_price_at_t <- function(iprice = 10, returns = 0.15,
                              volatility =0.2,time =1,steps =100){
-    while(steps > 0){
+  
+  while(steps > 0){
       
     #Calculate st + 1
     fprice = iprice * exp((
@@ -30,6 +34,24 @@ stock_price_at_t <- function(iprice = 10, returns = 0.15,
     steps = steps - 1
     count = count + 1
     }
-  message = paste("")
+  message = paste("A list of expected stock returns for ", count,
+                  " successive time intervals")
   print(message)
+  return(v_steps)
 }
+
+stock_price_at_t()
+wrDir = getwd()
+setwd()
+output = paste(wrDir,'Question 2.pdf')
+output
+
+generate.PDF <- function(data) {    
+  pdf(file=output, width=8.5, height=5,onefile=T)
+  dev.off()
+}
+generate.PDF(stock_price_at_t())
+
+pdf(file = output, title="Expected Stock Returns")
+stock_price_at_t()
+dev.off()
